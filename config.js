517779733f5047161be1a37b1274d9fd0b2c20da
simonhalas <simon.halas.sh@gmail.com>
2026-09-07 18:33:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Für den Start leer lassen: Die Seite liest data/articles.json.
 // Für gemeinsames Speichern später die Worker-URL eintragen, z.B.
 // window.WISSEN_API_URL = "https://wissen-api.DEINNAME.workers.dev";
-window.WISSEN_API_URL = "";
+window.WISSEN_API_URL = "https://hidden-glade-0d17.simon-halas-sh.workers.dev";
